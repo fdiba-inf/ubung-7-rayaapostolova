5f3880b1b1e48f04c5dfb3f437a17932efc74144
rayaapostolova <rajarus@abv.bg>
2020-11-10 16:41:17 +0000

    .

--- src/exercise7/Triangle.java
+++ src/exercise7/Triangle.java
@@ -13,10 +13,20 @@ public class Triangle {
             printLine(1, line);
         }
 
-        // Print the bottom part of the triangle that is under the longest line
+        for (int line = n - 1; line >= 1; line--) {
+          PrintLine(1, line);
+
+        }
     }
 
     public static void printLine(int start, int end) {
-        // Implement method
+        for (int i = start; i <= end; i++) {
+          System.out.print(i);
+          if (i < end) {
+            System.out.print(" ");
+          }
+        }
+
+        System.out.println();
     }
 }
